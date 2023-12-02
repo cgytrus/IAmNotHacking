14b5dc6b452e3194a955da2db6e10449860d1127
ConfiG <cgytrus@cgyt.ru>
2023-12-02 20:05:42 +0300

    fix condition

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,18 @@ using namespace geode::prelude;
 class $modify(PlayLayer) {
     void showEndLayer() {
         PlayLayer::showEndLayer();
-        if (m_isPracticeMode || m_level->m_stars < 8 || m_level->m_attempts >= 10 && m_level->m_jumps >= 50)
+        if (m_isPracticeMode || m_level->m_stars < 8 || m_level->m_attempts >= 10 || m_level->m_jumps >= 50)
             return;
-        bool antiAtts = m_level->m_attempts < 10;
-        bool antiJumps = m_level->m_jumps < 50;
         auto attsText = fmt::format("<cg>{}</c> {}", m_level->m_attempts.value(), m_level->m_attempts == 1 ? "attempt" : "attempts");
         auto jumpsText = fmt::format("<cb>{}</c> {}", m_level->m_jumps.value(), m_level->m_jumps == 1 ? "jump" : "jumps");
         geode::createQuickPopup(
             "Anti-cheat Warning",
             fmt::format(
-                "You have only achieved {} in this level, "
+                "You have only achieved <cg>{}</c> attempt{} and <cb>{}</c> jump{} in this level, "
                 "which will make its progress <cr>reset</c> once you <co>save</c> your progress to the cloud.\n"
-                "Do you want to <cy>alter</c> your current amount of {} to prevent that?",
-                antiAtts ? antiJumps ? fmt::format("{} and {}", attsText, jumpsText) : attsText : jumpsText,
-                antiAtts ? antiJumps ? "<cg>attempts</c> and <cb>jumps</c>" : "<cg>attempts</c>" : "<cb>jumps</c>"
+                "Do you want to <cy>alter</c> your current amount of <cg>attempts</c> and <cb>jumps</c> to prevent that?",
+                m_level->m_attempts.value(), m_level->m_attempts == 1 ? "" : "s",
+                m_level->m_jumps.value(), m_level->m_jumps == 1 ? "" : "s"
             ),
             "No",
             "Yes",
